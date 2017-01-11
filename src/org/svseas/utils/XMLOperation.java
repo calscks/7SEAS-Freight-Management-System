6a761d57d86d57e0ea88c75f57727e0557dcfcc9
lolzcalvin <ckseong96@hotmail.com>
2017-01-11 23:36:51 +0900

    Refresh v2

---
+++ src/org/svseas/utils/XMLOperation.java
@@ -0,0 +1,7 @@
+package org.svseas.utils;
+
+/**
+ * Codes by Seong Chee Ken on 11/01/2017, 23:35. This operation class uses JAXB as the medium between objects and XML.
+ */
+public class XMLOperation {
+}
