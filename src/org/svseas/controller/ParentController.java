7aa05f5f3f90f8c3fcd0f927eab1aa4649f43efe
lolzcalvin <brotherken1996@gmail.com>
2017-01-19 14:59:56 +0800

    Few changes

---
+++ src/org/svseas/controller/ParentController.java
@@ -0,0 +1,20 @@
+package org.svseas.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+
+import java.util.Objects;
+
+public class ParentController {
+    public boolean pwd_match(JFXPasswordField pass1, JFXPasswordField pass2){
+        if (Objects.equals(pass1.getText(), pass2.getText())){
+            return true;
+        } return false;
+    }
+
+    public boolean add(JFXButton addButton){
+        addButton.setOnMouseClicked(e->{
+
+        });
+    }
+}
--- src/org/svseas/controller/client/ClientAdd.java
+++ src/org/svseas/controller/client/ClientAdd.java
@@ -1,7 +1,24 @@
 package org.svseas.controller.client;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+import org.svseas.controller.ParentController;
+
+
 /**
  * Coded by Seong Chee Ken on 19/01/2017, 00:29.
  */
-public class ClientAdd {
+public class ClientAdd extends ParentController{
+    @FXML private StackPane clientadd_root;
+    @FXML private JFXButton btn_add;
+    @FXML private JFXTextField username, companyName, registry_no, phone_no;
+    @FXML private JFXPasswordField pwd, confirm_pwd;
+
+    @FXML
+    public void initialize(){
+
+    }
 }
